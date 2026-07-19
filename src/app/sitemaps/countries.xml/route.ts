bf02852b092c8b19f54743fcad0a435739796efa
rakibnuist <rakibnuist@gmail.com>
2026-07-19 23:23:55 +0600

    Update countries.xml sitemap to include all 11 active destination URLs

--- src/app/sitemaps/countries.xml/route.ts
+++ src/app/sitemaps/countries.xml/route.ts
@@ -1,8 +1,26 @@
 import { sitemapResponse } from '@/lib/sitemap-xml';
 
 export function GET() {
+  const activeCountries = [
+    'uk',
+    'hungary',
+    'south-korea',
+    'finland',
+    'malaysia',
+    'malta',
+    'cyprus',
+    'georgia',
+    'greece',
+    'croatia',
+    'thailand',
+  ];
+
   return sitemapResponse([
     { loc: 'https://eduexpressint.com/study-in-china-from-bangladesh', lastmod: '2026-07-19' },
     { loc: 'https://eduexpressint.com/bn/study-in-china', lastmod: '2026-07-19' },
+    ...activeCountries.map((slug) => ({
+      loc: `https://eduexpressint.com/destinations/${slug}`,
+      lastmod: '2026-07-19',
+    })),
   ]);
 }
